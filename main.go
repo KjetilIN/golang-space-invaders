404f0159d047f0e3bb1ae68bf4a0edf6037d5ef0
KjetilLaptop <kjetikin@stud.ntnu.no>
2022-08-10 17:21:29 +0200

    Add ebiten game engine

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+//Implement ebiten game engine 
+type Game struct{}
+
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
+func (g *Game) Update() error {
+    // Write your game's logical update.
+    return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+    // Write your game's rendering.
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+    return 320, 240
+}
 
 func main() {
-	fmt.Print("Test")
+    game := &Game{}
+    // Specify the window size as you like. Here, a doubled size is specified.
+    ebiten.SetWindowSize(640, 480)
+    ebiten.SetWindowTitle("Your game's title")
+    // Call ebiten.RunGame to start your game loop.
+    if err := ebiten.RunGame(game); err != nil {
+        log.Fatal(err)
+    }
 }
