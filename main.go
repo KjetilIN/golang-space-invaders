0922e2a90cd220633c459ed4886f98cd80fba4d1
KjetilLaptop <kjetikin@stud.ntnu.no>
2022-08-10 18:06:43 +0200

    Add sprite from sheet to jpeg

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"image/color"
 	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
-//Implement ebiten game engine 
+
+//Implement ebiten game engine
 type Game struct{}
 
 // Update proceeds the game state.
@@ -15,11 +17,13 @@ func (g *Game) Update() error {
     return nil
 }
 
+
 func (g *Game) Draw(screen *ebiten.Image) {
     // Write your game's rendering.
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.RGBA{0,0,0,0})
 }
 
+//Size of screen
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
     return 320, 240
 }
@@ -28,7 +32,7 @@ func main() {
     game := &Game{}
     // Specify the window size as you like. Here, a doubled size is specified.
     ebiten.SetWindowSize(640, 480)
-    ebiten.SetWindowTitle("Your game's title")
+    ebiten.SetWindowTitle("Space Invaders 2022 - TekKom")
     // Call ebiten.RunGame to start your game loop.
     if err := ebiten.RunGame(game); err != nil {
         log.Fatal(err)
